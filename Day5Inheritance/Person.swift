14c803516c621fc564da1d84a8f6ae9bdc8753ad
MacStudent <macstudent@126-iMac12.local>
2019-02-14 18:56:40 -0500

    person class added

---
+++ Day5Inheritance/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Day5Inheritance
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Person
+{
+    
+}
