40e1e7e95d4ffde0c9f4071a87fdd48ee8cf67a7
MacStudent <macstudent@126-iMac12.local>
2019-02-14 18:25:00 -0500

    Initial Commit

---
+++ Day5Inheritance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day5Inheritance
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
